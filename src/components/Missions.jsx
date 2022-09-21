d659fb77c7d8864d1964905b6c4b02df6ed1d24c
petromamboubee <gm.boubee@gmail.com>
2022-09-21 14:38:59 -0300

    tentando fazer um bom css

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -6,16 +6,18 @@ import missions from '../data/missions';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className="missão1">
         <Title headline="Missões" />
-        {missions
-          .map((element) => (<MissionCard
-            key={ element.name }
-            name={ element.name }
-            year={ element.year }
-            country={ element.country }
-            destination={ element.destination }
-          />))}
+        <div className="missão">
+          {missions
+            .map((element) => (<MissionCard
+              key={ element.name }
+              name={ element.name }
+              year={ element.year }
+              country={ element.country }
+              destination={ element.destination }
+            />))}
+        </div>
       </div>
     );
   }
